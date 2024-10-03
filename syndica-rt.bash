c4bf1984c7e143c47179b5f3e90a601316c8ab31
Dalir Absent <dalirabsent@gmail.com>
2024-10-03 19:41:49 +0330

    Syndica bash theme for non-graphical terminal

---
+++ syndica-rt.bash
@@ -0,0 +1,59 @@
+function command_prompt()
+{
+    local exit_code=$?
+    local reset_color="\[\e[0;38;5;255m\]"
+    local user_host="\[\e[1;38;5;33m\]\u@\h$reset_color"
+    local current_dir="\[\e[1;38;5;10m\]\w$reset_color"
+    local user_symbol="\[\e[1;38;5;255m\]\$$reset_color"
+    local error_code=""
+    local git_info=""
+    local venv_info=""
+
+    if [ $exit_code -ne 0 ]; then
+        error_code="\[\e[1;38;5;1m\]<$exit_code>$reset_color"
+    fi
+
+    if [ $(git rev-parse --is-inside-work-tree 2> /dev/null) ]; then
+        git_info="$(git_prompt)$reset_color"
+    fi
+
+    if [ -n "$VIRTUAL_ENV" ]; then
+        venv_info="$(venv_prompt)$reset_color"
+    fi
+
+    PS1="/--($user_host)-[$current_dir]$venv_info$git_info $error_code\n\\--$user_symbol "
+}
+
+
+function git_prompt()
+{
+    local branch=$(git rev-parse --abbrev-ref HEAD)
+    local status=$(git status --porcelain)
+
+    local reset_color="\[\e[1;38;5;255m\]"
+    local dirty=""
+
+    if [ -n "$status" ]; then
+        dirty="\[\e[1;38;5;196m\]*$reset_color"
+    fi
+
+    local prompt="$reset_color->(\[\e[38;5;93m\]git:$reset_color[\e[38;5;214m\]$branch$reset_color] $dirty )"
+
+    echo "$prompt"
+}
+
+
+function venv_prompt()
+{
+    local venv_name=$(basename $VIRTUAL_ENV)
+
+    local reset_color="\[\e[1;38;5;255m\]"
+
+    local prompt="$reset_color{\[\e[1;38;5;69m\]$venv_name$reset_color}"
+
+    echo "$prompt"
+}
+
+
+
+PROMPT_COMMAND=command_prompt
